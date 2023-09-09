f4cff1a53555f7eb820c7c166203d7305cbcf2ad
schelpstech <69238965+schelpstech@users.noreply.github.com>
2023-09-09 21:42:42 +0100

    update

--- view/include/header.php
+++ view/include/header.php
@@ -18,7 +18,7 @@
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:200,300,400,400i,500,600,700%7CMerriweather:300,300i" rel="stylesheet">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     </head>
-    <body class=" ">
+    <body class=" " style="zoom: 100%;">
         <a id="start"></a>
         <section class="bar bar-3 bar--sm bg--secondary">
             <div class="container">
